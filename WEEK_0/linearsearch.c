7a8f03320f557511e99dbdc7c3e2ac792be94b37
yogeshkumar71 <yogeshkumar.cs24@bmsce.ac.in>
2026-03-13 20:33:18 +0530

    Create linearsearch.c

---
+++ WEEK_0/linearsearch.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(){
+printf("USN:1BF24CS337\n");
+int i,n,item;
+int a[10];
+int pos=-1;
+printf("Enter the lenght of elements: ");
+scanf("%d",&n);
+for(i=0;i<n;i++){
+    printf("\nEnter the elements: ");
+    scanf("%d",&a[i]);
+}
+printf("\nEnter the element to be searched: ");
+scanf("%d",&item);
+for (i=0;i<n;i++){
+    if(item==a[i])
+        pos=i;
+}
+if(pos!=-1)
+    printf("\nElement found at %d",pos);
+else
+    printf("\nElement not found");
+return 0;
+}
